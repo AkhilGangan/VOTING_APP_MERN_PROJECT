e2a60c5292522e9fcf0e4c6eea091438050a1ad3
Akhil <akhilg7162@gmail.com>
2023-08-03 19:07:05 +0530

    Updated Files

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const  mongoose  = require("mongoose");
 const bcrypt = require('bcryptjs');
 
@@ -17,17 +18,37 @@ const userSchema = new mongoose.Schema({
     phone:{
         type:Number,
         required:true
-    }
+    },
+    tokens:[
+        {
+            token:{
+                type:String,
+                required:true
+            }
+        }
+    ]
 })
 
 //We are hashing the password
-userSchema.pre ('save',async function (next){
+userSchema.pre('save',async function (next){
     //console.log("Hii from inside");
     if(this.isModified("password")){
-        this.password=await bcrypt.hash(this.password,12);
+        this.password = await bcrypt.hash(this.password,12);
     }
     next();
 });
+
+// We are generating token
+userSchema.methods.generateAuthToken = async function(){
+    try{
+        let token = jwt.sign({_id:this._id},process.env.SECRET_KEY);
+        this.tokens = this.tokens.concat({token:token});
+        await this.save();
+        return token;
+    }catch(err){
+        console.log(err);
+    }
+} 
 
 const User = mongoose.model('USER',userSchema);
 
--- server/router/auth.js
+++ server/router/auth.js
@@ -1,72 +1,82 @@
-const express = require('express');
+const jwt = require("jsonwebtoken");
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs')
-const User = require('../model/userSchema');
-require('../db/conn');
+const bcrypt = require("bcryptjs");
 
+require("../db/conn");
+const User = require("../model/userSchema");
 
-router.get('/',(req,res)=>{
-    res.send('Hello World from Akhil Router js');
+router.get("/", (req, res) => {
+  res.send("Hello World from Akhil Router js");
 });
 
 //register route
-router.post('/register',(req,res)=>{
-    console.log(req.body);
-    // res.json({message:req.body});
-    //res.send("My register page");
-const {username,password,email,phone} = req.body 
+router.post("/register", (req, res) => {
+  console.log(req.body);
+  // res.json({message:req.body});
+  //res.send("My register page");
+  const { username, password, email, phone } = req.body;
 
-  if(!username || !password || !email|| !phone ){
-     return res.status(422).json({error:"please add all the fields"})
+  if (!username || !password || !email || !phone) {
+    return res.status(422).json({ error: "please add all the fields" });
   }
-  
-  User.findOne({email:email})
-  .then((userExist)=>{
-      if(userExist){
-        return res.status(422).json({error:"user already exists with that email"})
+
+  User.findOne({ email: email })
+    .then((userExist) => {
+      if (userExist) {
+        return res
+          .status(422)
+          .json({ error: "user already exists with that email" });
       }
-      const user = new User({username,password,email,phone});
+      const user = new User({ username, password, email, phone });
 
-      user.save().then(()=>{
-        res.status(201).json({message:"user registered successfully"});
-      }).catch(err=>{res.status(500).json({error:"Failed to register"})
+      user.save().then(() => {
+          res.status(201).json({ message: "user registered successfully" });
+        })
+        .catch((err) => {
+          res.status(500).json({ error: "Failed to register" });
+        });
+    })
+    .catch((err) => {
+      console.log(err);
     });
-
-}).catch(err=>{console.log(err)});
 });
 
 //login route
-router.post('/Signin',async(req,res)=>{
-    console.log(req.body)
-    try{
-        const {username,password} = req.body
-        if(!username || !password){
-            return res.status(422).json({error:"please add user or password"})
-         }
-         const userLogin = await User.findOne({username:username});
-         console.log(userLogin);
-
-         if (userLogin){
-           const isMatch=await bcrypt.compare(password,userLogin.password);
-           if(!isMatch){
-            res.status(400).json({error:"Invalid credential pas"});
-           }
-           else{
-            res.json({message:"user sign in successful"});
-            }
-        }else{
-            res.status(400).json({error:"Invalid credentials"});
-        }
-    }catch(err){
-          console.log(err)
+router.post("/signin", async (req, res) => {
+//   console.log(req.body);
+  try {
+    let token;
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res.status(422).json({ error: "please add user or password" });
     }
-});
-   
+    const userLogin = await User.findOne({ username: username });
+    //  console.log(userLogin);
 
-    // User.findOne({email:email})
-    // .then(userExist=>{
-    //   if(!userExist){
-    //      return res.status(422).json({error:"Invalid Email or password"})
-    //   }
+    if (userLogin) {
+      const isMatch = await bcrypt.compare(password, userLogin.password);
+
+      token = await userLogin.generateAuthToken();
+      console.log(token);
+
+      if (!isMatch) {
+        res.status(400).json({ error: "Invalid credential pas" });
+      } else {
+        res.json({ message: "user sign in successful" });
+      }
+    } else {
+      res.status(400).json({ error: "Invalid credentials" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// User.findOne({email:email})
+// .then(userExist=>{
+//   if(!userExist){
+//      return res.status(422).json({error:"Invalid Email or password"})
+//   }
 
 module.exports = router;
